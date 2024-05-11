6e65285bdaa7ee5cdaa38354496a50124ee86c91
Gabriel Senra <gabriel_447@live.com>
2024-05-11 20:58:46 -0300

    comments closure

--- chainedfn.php
+++ chainedfn.php
@@ -1,5 +1,9 @@
 <?php
 
+//esse codigo pode ser considerado uma closure 
+//porque usa uma variavel global chamada $op 
+//que e fornecida pela funcao anonima e reutilizada pela nomeclatura use
+
 function executar($a, $b, $op) {
     $func = function($a, $b) use ($op) {
         switch ($op) {
