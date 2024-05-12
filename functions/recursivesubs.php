632ad1cef100a2ae241f8d6736a48b00b9816739
Gabriel Senra <gabriel_447@live.com>
2024-05-12 19:16:14 -0300

    recursive sum n subs

---
+++ functions/recursivesubs.php
@@ -0,0 +1,12 @@
+<?php
+
+function recursiveFunction($num)
+{
+    if ($num!= 0) {
+        echo "$num ";
+        recursiveFunction(--$num);
+    }
+}
+recursiveFunction(10);
+
+?>
---
+++ functions/recursivesum.php
@@ -0,0 +1,12 @@
+<?php
+
+function recursiveFunction($num)
+{
+    if ($num != 100) {
+        echo "$num ";
+        recursiveFunction(++$num);
+    }
+}
+recursiveFunction(10);
+
+?>
