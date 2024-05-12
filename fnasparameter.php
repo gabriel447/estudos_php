3886832e0d0f1df2c99c3107b6ce91e884766af8
Gabriel Senra <gabriel_447@live.com>
2024-05-11 21:00:09 -0300

    funcao passada como parametro de outra

---
+++ fnasparameter.php
@@ -0,0 +1,22 @@
+<?php
+
+$func = function($a, $b, $op) {
+    switch ($op) {
+        case '+':
+            return $a + $b;
+        case '-':
+            return $a - $b;
+        case '*':
+            return $a * $b;
+        case '/':
+            return $a / $b;
+    }
+};
+
+function executar($a, $b, $op, $func) {
+	echo $func($a, $b, $op);
+}
+
+executar(11, 11, '+', $func);
+
+?>
