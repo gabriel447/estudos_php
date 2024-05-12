0cb3b030498ff1929e8c963411f165513df0ff96
Gabriel Senra <gabriel_447@live.com>
2024-05-11 21:11:06 -0300

    sortear valores na consulta em array com rand

---
+++ arrays/randtosort.php
@@ -0,0 +1,18 @@
+<?php
+
+$sonhoviajar = array(
+    'Noruega',
+    'Suiça',
+    'Amsterdam',
+    'Tailandia',
+    'Japao',
+    'California'
+);
+
+$indiceSorteado = rand(0, count($sonhoviajar) - 1);
+
+$paisSorteado = $sonhoviajar[$indiceSorteado];
+
+echo "O país sorteado para sua próxima viagem é: ". $paisSorteado;
+
+?>
