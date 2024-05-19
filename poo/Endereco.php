b0826ec2117e851a6b34d9534cab9cd85ce8ff31
Gabriel Senra <gabriel_447@live.com>
2024-05-19 16:58:15 -0300

    construct e destruct

---
+++ poo/Endereco.php
@@ -0,0 +1,28 @@
+<?php
+
+class Endereco {
+    private $bairro;
+    private $cidade;
+    private $estado;
+
+    public function __construct($bairro, $cidade, $estado = 'Parana') {
+        $this->bairro = $bairro;
+        $this->cidade = $cidade;
+        $this->estado = $estado;
+    }
+
+    public function __destruct() {
+        $this->bairro = null;
+        $this->cidade = null;
+        $this->estado = null;
+    }
+
+    public function getAtributos() {
+        echo "Bairro: {$this->bairro}, Cidade: {$this->cidade}, Estado: {$this->estado}";
+    }
+}
+
+$endereco = new Endereco('Xaxim', 'Curitiba');
+$endereco->getAtributos();
+unset($endereco);
+$endereco->getAtributos();
