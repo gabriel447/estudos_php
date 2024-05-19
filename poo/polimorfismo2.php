0839c8c9a3e8a39999d5dfd064c48c9986f36043
Gabriel Senra <gabriel_447@live.com>
2024-05-19 18:31:05 -0300

    polimorfismo

--- poo/polimorfismo2.php
+++ poo/polimorfismo2.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+Neste exemplo, 
+o polimorfismo permite que o método demonstrarHabilidade() 
+seja chamado de maneiras diferentes em objetos de diferentes classes (Professor e Aluno), 
+mesmo que o método tenha a mesma assinatura em todas elas. 
+Isso mostra como o polimorfismo pode ser utilizado para criar interfaces flexíveis e reutilizáveis em PHP, 
+permitindo que o mesmo método seja adaptado para diferentes contextos e comportamentos. 
+*/
+
 abstract class Pessoa {
     abstract public function demonstrarHabilidade();
 }
