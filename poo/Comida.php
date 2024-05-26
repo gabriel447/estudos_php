6efe06035b5e68582ca7db27ab08ada7a1afe3a9
Gabriel Senra <gabriel_447@live.com>
2024-05-26 20:36:09 -0300

    polimorfismo comida

---
+++ poo/Comida.php
@@ -0,0 +1,42 @@
+<?php
+
+abstract class Comida {
+    public $peso;
+}
+
+class Arroz extends Comida {
+}
+
+class Sorvete extends Comida {
+
+}
+
+class Banana extends Comida {
+
+}
+
+class Pessoa {
+    public $peso;
+
+    function __construct($peso) {
+        $this->peso = $peso;
+    }
+
+    public function comer(Comida $comida) {
+        $this->peso += $comida->peso;
+    }
+}
+
+$arroz = new Arroz();
+$arroz->peso = 0.66;
+
+$sorvete = new Sorvete();
+$sorvete->peso = 1.5;
+
+$banana = new Sorvete();
+$banana->peso = 0.30;
+
+$pessoa = new Pessoa(79.5);
+$pessoa->comer($banana);
+
+echo $pessoa->peso;
