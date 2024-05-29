1958344a2f3711537a015a7af9ece9947e2dc872
Gabriel Senra <gabriel_447@live.com>
2024-05-29 20:08:59 -0300

    mm

---
+++ mm.php
@@ -0,0 +1,24 @@
+<!-- 
+
+Métodos Mágicos
+    
+__construct
+seta o valor dos os atributos da classe através dos parametros 
+passados ao instânciar...
+
+__destruct 
+destrói a instancia da classe quando utilizado o 'unset'.
+
+__get
+é acionado quando você tenta buscar um atributo que 
+não foi criado na classe.
+
+__set
+é acionado quando você tenta atribuir valor á um atributo
+que não existe na classe.
+
+__call
+é acionado quando você tenta acessar um método que não existe
+na classe. 
+
+-->
