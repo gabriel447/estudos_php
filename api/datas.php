3718f1ea98fa97649ae45dff54dd2fc584266828
Gabriel Senra <gabriel_447@live.com>
2024-06-01 09:48:45 -0300

    date

--- api/datas.php
+++ api/datas.php
@@ -1,4 +1,15 @@
 <?php
 
-//https://www.php.net/manual/pt_BR/datetime.gettimestamp.php
-//https://www.php.net/manual/pt_BR/class.datetime.php
+$dataHora = new DateTime('2024-06-01');
+echo $dataHora->format('d-m-Y')."\n";
+
+$dataHora->modify('+1 month');
+echo $dataHora->format('d-m-Y')."\n";
+
+$intervalo = new DateInterval('P10D');
+$dataHora->add($intervalo);
+echo $dataHora->format('d-m-Y')."\n";
+
+$fusoHorarioBrasilia = new DateTimeZone('America/Sao_Paulo');
+$dataHora->setTimezone($fusoHorarioBrasilia);
+echo $dataHora->format('d-m-Y H:i:s T')."\n";
